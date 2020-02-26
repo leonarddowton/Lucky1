ee43eb24a4980acde775d3c697ced1c324972295
leonarddowton <leonarddowton.53@gmail.com>
2020-02-26 16:57:21 +1100

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int k;
+    printf("Enter an integer: ");
+    scanf("%d", &k);
+    k = 2*k;
+    printf("That integer doubled is: %d\n", k);
+    return 0;
+}
